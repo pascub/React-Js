f98d8b99a6bbf6df9536358cfe7392b89389f9a2
Renzo Bonavita <pascualbonavita1@gmail.com>
2022-08-30 22:20:21 -0300

    Navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import logo from '../logo.svg'
+
+const NavBar = () => {
+
+    return (
+    <div>
+        
+        <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+        <div class="diiv">
+            <h2>Greenpots</h2>
+        </div>  
+    
+    </div>);
+};
+
+
+
+
+export default NavBar;
